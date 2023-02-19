34c328cc473a53b427144ec83635712170e761ab
zbndm <zbndmt@gmail.com>
2023-02-19 07:49:33 +0200

    -

--- prettier.config.js
+++
@@ -1,4 +0,0 @@
-GOOGLE_CLIENT_ID="262918829565-hu8j80alblviin7nshbq7hqrnb4buqlc.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET="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"
-DATABASE_URL=postgresql://o9:TgFtBKrhqxTCpuo0Y_lN2Q@lllllll-3389.6zw.cockroachlabs.cloud:26257/x?sslmode=verify-full
-NEXTAUTH_SECRET=supersecret
